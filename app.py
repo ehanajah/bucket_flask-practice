d4b99765f322cb96b69f32d55928d1659d83af8f
ehanajah <rnajahazmy@gmail.com>
2023-10-11 12:09:01 +0800

    add .env file, fix db configuration ind app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("mongodb://test:sparta@ac-2qr5raf-shard-00-00.jeuk15y.mongodb.net:27017,ac-2qr5raf-shard-00-01.jeuk15y.mongodb.net:27017,ac-2qr5raf-shard-00-02.jeuk15y.mongodb.net:27017/?ssl=true&replicaSet=atlas-f4ty2t-shard-0&authSource=admin&retryWrites=true&w=majority")
-DB_NAME =  os.environ.get("dbsparta")
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
-db = client(DB_NAME)
+db = client[DB_NAME]
 
 app=Flask(__name__)
 
